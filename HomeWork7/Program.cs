106191757c47355e9f9403746a78411f6d52dd69
fyodor <fyodor.zakharov@gmail.com>
2021-03-14 00:23:06 +0300

    Add project files.

---
+++ HomeWork7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HomeWork7
+     /*Write a C# Sharp program to create a new list of the rightmost digits
+      from a given list of positive integers.*/
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+            var inputList = new List<int>();
+            
+
+            var rndm = new Random();
+            for (int i = 0; i < (rndm.Next(6)+5); i++) //количество элементов списка от 5 до 15
+            {
+                inputList.Add(rndm.Next(999));
+            }
+
+            Console.WriteLine("Given list: " + string.Join(", ", inputList));
+
+            var outputList = inputList.Select(i => i % 10).ToList();
+
+            Console.WriteLine("Output list:  " + string.Join(", ", outputList));
+        }
+    }
+}
